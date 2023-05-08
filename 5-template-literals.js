dc759b3ff6c7e6e2eb19482ac5ade65e502b9338
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-08 17:41:10 +0300

    Define sum function in one line

--- 5-template-literals.js
+++ 5-template-literals.js
@@ -6,10 +6,14 @@ console.log(sayHi('Dan'));
 
 const baseUrl = 'https://swapi.co/api';
 
-fetch(`${baseUrl}/people/1`)
-  .then(res => res.json)
-  .then(json => console.log(json));
+// fetch(`${baseUrl}/people/1`)
+//   .then(res => res.json)
+//   .then(json => console.log(json));
 
-fetch(`${baseUrl}/people/2`)
-  .then(res => res.json)
-  .then(json => console.log(json));
+// fetch(`${baseUrl}/people/2`)
+//   .then(res => res.json)
+//   .then(json => console.log(json));
+
+const sum = (a, b) => `Sum: ${a + b}`;
+
+console.log(sum(1,3));
