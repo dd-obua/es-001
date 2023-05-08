962ffd5e1f3b23c334c41f20f68817ef449e39f5
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-08 16:30:58 +0300

    Show const variable scope

--- 1-variables.js
+++ 1-variables.js
@@ -10,3 +10,17 @@ for (let i = 0; i < 3; i++) {
 }
 
 // console.log('Outside:', i); // i is undefined at this point
+
+// Show const variable scope
+function checkPassword(password) {
+  const valid = password.length > 6;
+  if (valid) {
+    const message = 'Your password is valid.';
+    console.log(message);
+  } else {
+    const message = 'Your password is not valid.';
+    console.log(message);
+  }
+}
+checkPassword('123');
+checkPassword('1234567');
