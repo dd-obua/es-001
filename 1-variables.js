9e5249c5d729814401010ecc919f4c9348ad4ec4
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-08 16:29:29 +0300

    Show scope of let variables

--- 1-variables.js
+++ 1-variables.js
@@ -2,3 +2,10 @@
 // Reassign let variable
 let name = 'Denis';
 name = 'Dan';
+
+// Show scope of let variables
+for (let i = 0; i < 3; i++) {
+  console.log('Inside:', i);
+}
+
+console.log('Outside:', i);
