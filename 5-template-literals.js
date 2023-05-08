a08e83171af4055c1ee39927f3b3377c37ca6d86
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-08 17:39:06 +0300

    Replace base url

--- 5-template-literals.js
+++ 5-template-literals.js
@@ -3,3 +3,13 @@
 const sayHi = name => `Hi ${name}.`;
 
 console.log(sayHi('Dan'));
+
+const baseUrl = 'https://swapi.co/api';
+
+fetch(`${baseUrl}/people/1`)
+  .then(res => res.json)
+  .then(json => console.log(json));
+
+fetch(`${baseUrl}/people/2`)
+  .then(res => res.json)
+  .then(json => console.log(json));
