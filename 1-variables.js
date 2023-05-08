6f4803b559c3a1c614db4c2a7fab822e6916c4d7
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-08 16:30:40 +0300

    i is not accessible outside its block

--- 1-variables.js
+++ 1-variables.js
@@ -4,8 +4,9 @@ let name = 'Denis';
 name = 'Dan';
 
 // Show scope of let variables
+// Log current index
 for (let i = 0; i < 3; i++) {
   console.log('Inside:', i);
 }
 
-console.log('Outside:', i);
+// console.log('Outside:', i); // i is undefined at this point
